58d8db30440d0ddf54d5eaa0413f957cdf9550b2
Filipe Simões Teodoro <68706037+fstufop@users.noreply.github.com>
2022-09-13 23:06:41 -0300

    🌟: project organized

---
+++ FSTBaseProject/FSTBaseProject/Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  FSTBaseProject
+//
+//  Created by Filipe Simoes Teodoro on 13/09/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let viewController = VIPViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
