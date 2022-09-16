aea9912ea114470566c136b609d3715ac8563c1e
Filipe Simões Teodoro <68706037+fstufop@users.noreply.github.com>
2022-09-16 13:17:57 -0300

    Create AlertViewModel.swift

---
+++ FSTBaseProject/FSTBaseProject/Scene/AlertViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AlertViewModel.swift
+//  FSTBaseProject
+//
+//  Created by Filipe Simoes Teodoro on 16/09/22.
+//
+
+import Foundation
+
+struct AlertViewModel {
+    let title: String
+    let message: String
+    let buttonTitle: String
+}
